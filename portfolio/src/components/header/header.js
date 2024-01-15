391a7770ea34f91417142aa5d66cd1292bd697c4
SparkyGuy <tur.roberto456@gmail.com>
2024-01-14 22:21:18 -0300

    first commit

---
+++ portfolio/src/components/header/header.js
@@ -0,0 +1,23 @@
+import './header.css';
+import { VscGithub } from "react-icons/vsc";
+import { CgMail } from "react-icons/cg";
+
+function Header() {
+    return (
+      <div className="header-container">
+       <div className='topics'>
+            <div className='topics-item'><h5>Arthur</h5></div>
+            <div className='topics-item'><h5>Sobre Mim</h5></div>
+            <div className='topics-item'><h5>Projetos</h5></div>
+            <div className='topics-item'><h5>Contato</h5></div>            
+       </div>
+            <div className='icons-container'>
+                <div className='icon' >
+                    <a href="https://github.com/SparkyGuy"><VscGithub size="40px" color="#B6B6B6"/></a>
+                </div>
+            </div>
+      </div>
+    );
+  }
+
+export default Header;
---
+++ portfolio/src/main.js
@@ -0,0 +1,19 @@
+import './main.css';
+import Header from './components/header/header'
+
+function Main() {
+  return (
+    <div className="main">
+     <Header/>
+     <div className='container'>
+        <div className='light-text'>
+          <h3>OLÁ, MEU NOME É</h3>
+          <h1>ARTHUR ROBERTO</h1>
+        </div>
+    </div>
+    </div>
+
+  );
+}
+
+export default Main;
